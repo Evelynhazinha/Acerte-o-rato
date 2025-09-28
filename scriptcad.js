54bff499c50ed5f66dee0628a60fa8abe47e784f
Évelyn <evelynsoliveira.ifes@gmail.com>
2025-09-27 21:17:57 -0300

    uptade

--- scriptcad.js
+++ scriptcad.js
@@ -1,10 +1,13 @@
-document.getElementById.addEventListener("submit", function (event) {
+document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
     const nome = document.getElementById("nome").value.trim();
     const email = document.getElementById("email").value.trim();
     const senha = document.getElementById("senha").value.trim();
     if (nome === "" || email === "" || senha === "") {
         alert("Todos os campos são obrigatórios!");
+        nome.style.border="2vh solid red"
+        email.style.border="2vh solid red"
+        senha.style.border="2vh solid red"
         return;
     }
     if (senha.length < 6) {
